4651975ff44a2e61ee0002e8adfc122854960064
João Vitor dos Santos <dev.joaovitor1@gmail.com>
2024-01-31 23:12:14 -0300

    Criação da repository genérica

---
+++ Domain/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> Get(int id);
+
+        Task Add(T item);
+
+        Task Edit(T item);
+
+        Task Delete(int id);
+    }
+}
