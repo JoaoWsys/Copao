a45c933536021f0ac83e28e7a2a1c0eceac5d7ee
João Vitor dos Santos <dev.joaovitor1@gmail.com>
2024-01-31 23:37:09 -0300

    Criação do projeto infrastructure

---
+++ Infrastructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UsuarioRepository : IRepository<Usuario>
+    {
+        private CopaoDbContext _db;
+        public Task Add(Usuario item)
+        {
+            _db.Users.Add(item);
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(Usuario item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Usuario>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
